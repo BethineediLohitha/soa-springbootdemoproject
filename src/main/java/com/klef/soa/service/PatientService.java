3179a3d04e4249c50d9df601fc6a4948413fa782
lohitha chowdary bethineedi <your-github-email@gmail.com>
2026-07-27 22:04:34 +0530

    Project v3.0

--- src/main/java/com/klef/soa/service/PatientService.java
+++ src/main/java/com/klef/soa/service/PatientService.java
@@ -6,14 +6,11 @@ import com.klef.soa.entity.Patient;
 
 public interface PatientService 
 {
-	Patient  addPatient(Patient patient);
-	List<Patient> displayAllPatients();
-	Patient updatePatient(Patient p);
-	Patient displayPatientbyId(Long id);
-	//save performs 2 ops insert and update
-	//list,one object,string --> these are only the return types
-	String deletePatientById(Long id);
-	List<Patient> displayPatientByGender(String gender);
-	
-
+  Patient addPatient(Patient patient);
+  List<Patient> displayAllPatients();
+  Patient updatePatient(Patient p);
+  Patient displayPatientById(Long id);
+  String deletePatientById(Long id);
+  List<Patient> displayPatientsByGender(String gender);
+  Long displayPatientCount();
 }
--- src/main/java/com/klef/soa/service/PatientServiceImpl.java
+++ src/main/java/com/klef/soa/service/PatientServiceImpl.java
@@ -8,86 +8,73 @@ import org.springframework.stereotype.Service;
 
 import com.klef.soa.entity.Patient;
 import com.klef.soa.repository.PatientRepository;
+
 @Service
-public class PatientServiceImpl implements PatientService 
+public class PatientServiceImpl implements PatientService
 {
-	
-	
-	@Autowired
-	private PatientRepository repo;
-	//autowired is the concept of communication b/w two managed objects
+  @Autowired
+  private PatientRepository repo;
+  @Override
+  public Patient addPatient(Patient patient) 
+  {
+    return repo.save(patient);
+  }
 
-	@Override
-	public Patient addPatient(Patient patient) 
-	{	
-		return repo.save(null);
-	}
+  @Override
+  public List<Patient> displayAllPatients() {
+    return repo.findAll();
+  }
 
-	
-	
-	
-	@Override
-	public List<Patient> displayAllPatients() 
-	{
-		return repo.findAll();
-	}
+  @Override
+  public Patient updatePatient(Patient p) 
+  {
+  Optional<Patient> optional =repo.findById(p.getId());
+  if(optional.isPresent()) 
+  {
+    Patient patient = optional.get();
+    patient.setName(p.getName());
+    patient.setAge(p.getAge());
+    patient.setContact(p.getContact());
+    patient.setRemarks(p.getRemarks());
+    
+    return repo.save(patient);
+    
+  }
+  else {
+    return null;
+  }
+  }
 
-	@Override
-	public Patient updatePatient(Patient p) 
-	{
-		Optional<Patient> optional = repo.findById(p.getId());
-		if(optional.isPresent())
-		{
-			Patient patient = optional.get();
-			//here we have two patient objects pa nd patient --> p is the input and patient is the existing object
-			patient.setName(p.getName());
-			patient.setAge(p.getAge());
-			patient.setContact(p.getContact());
-			patient.setRemarks(p.getRemarks());
-			return repo.save(patient);
-		}
-		else
-		{
-			return null;
-		}
-	}
+  @Override
+  public Patient displayPatientById(Long id) 
+  {
+    return repo.findById(id).orElse(null);
+  }
 
+  @Override
+  public String deletePatientById(Long id) {
+    boolean status =repo.existsById(id);
+    if(status) 
+    {
+      repo.deleteById(id);
+      return "Patient Deleted Successfully";
+    }
+    else 
+    {
+      return "Patient ID Not Found";
+    }
+  }
 
-	
-	
-	
-	@Override
-	public Patient displayPatientbyId(Long id) 
-	{
-		return repo.findById(id).orElse(null);//findby id give the return type as optional class
-	}
-//optional class is container that may or maynot give the object--it can be checked by isclass
-	
-	
-	
-	
-	
-	@Override
-	public String deletePatientById(Long id) 
-	{
-		boolean status = repo.existsById(id);
-		if(status)
-		{
-			repo.deleteById(id);
-			return "Patient Deleted Successfully";
-		}
-		else
-		{
-			return "Patient Id not Found";
-		}
-	}
+  @Override
+  public List<Patient> displayPatientsByGender(String gender)
+  {
+    return repo.findByGender(gender);
+  }
 
-	
-	
-	@Override
-	public List<Patient> displayPatientByGender(String gender) 
-	{
-		return repo.findByGender(gender);
-	}
+  @Override
+  public Long displayPatientCount()
+  {
+	return repo.count();
+  }
 
 }
